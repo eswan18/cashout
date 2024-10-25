b9a295315f424ef5e778e77b53dac0b21db66343
Ethan Swan <ethanpswan@gmail.com>
2024-10-25 15:56:17 -0500

    Start a share sheet.

--- app/games/[gameId]/game-link-panel.tsx
+++ app/games/[gameId]/game-link-panel.tsx
@@ -15,18 +15,22 @@ export default function GameLinkPanel({ gameId }: { gameId: string }) {
       });
     });
   };
-  return (
-    <div className="flex flex-row gap-1 text-xs items-center">
-      <div className="text-muted-foreground">Share Link:</div>
-      <Button
-        size="sm"
-        variant="ghost"
-        className="font-mono px-1 text-xs"
-        onClick={handleCopy}
-      >
-        <p>{url}</p>
-        <ClipboardCopy size={14} />
-      </Button>
-    </div>
-  );
+  // Check if navigator has a canShare method
+  const canShare = navigator["canShare"] ? navigator.canShare() : false;
+  return canShare
+    ? <div>you can share!</div>
+    : (
+      <div className="flex flex-row gap-1 text-xs items-center">
+        <div className="text-muted-foreground">Share Link:</div>
+        <Button
+          size="sm"
+          variant="ghost"
+          className="font-mono px-1 text-xs"
+          onClick={handleCopy}
+        >
+          <p>{url}</p>
+          <ClipboardCopy size={14} />
+        </Button>
+      </div>
+    );
 }
